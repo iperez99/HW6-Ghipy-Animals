106bfecad0fdd78c85b78c01fb1af12a22843c96
Qbisst <ismaelaperez@outlook.com>
2019-03-05 19:35:17 -0500

    finishing touches

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,37 @@
-//set variable for inital array of searches//
-var initialSearch = ['Shark', 'Panther', 'Elephant'];
+
+//Array that will show the initial buttons on the page.//
+var initialSearch = ["Shark", "Panther", "Elephant"];
+
+//Function to add buttons into page//
+function createButtons(initialSearch, classToAdd, placeToAdd) {
+
+    $(placeToAdd).empty();
+
+    for (var i = 0; i < initialSearch.length; i++) {
+
+        var newButton = $("<button>");
+
+        newButton.addClass(classToAdd);
+        //add the attribute from the array into the button/
+        newButton.attr("data-type", initialSearch[i]);
+        //adds an animal name from the array into new button//
+        newButton.text(initialSearch[i]);
+        //appends the button to html page//
+        $(placeToAdd).append(newButton);
+
+
+
+
+    }
+}
+
+//this function calls the initialSearch array and adds a class of 'searchButton' to each array item,//
+// then injects the buttons to the html page//
+
+$(function () {
+
+    createButtons(initialSearch, "searchButton", "#buttons-list")
+
+});
 
 
