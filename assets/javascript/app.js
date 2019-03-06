aa8dad0f46d1ec4a2cdd82acbb3cb01018f1f23b
Qbisst <ismaelaperez@outlook.com>
2019-03-05 20:19:20 -0500

    finishing touches

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,7 @@ var initialSearch = ["Shark", "Panther", "Elephant"];
 
 //Function to add buttons into page//
 function createButtons(initialSearch, classToAdd, placeToAdd) {
-
+    //this function VV clears the search box after the name is added in the gif list//
     $(placeToAdd).empty();
 
     for (var i = 0; i < initialSearch.length; i++) {
@@ -12,15 +12,12 @@ function createButtons(initialSearch, classToAdd, placeToAdd) {
         var newButton = $("<button>");
 
         newButton.addClass(classToAdd);
-        //add the attribute from the array into the button/
+        //add the data type attribute from the items array into the button//
         newButton.attr("data-type", initialSearch[i]);
         //adds an animal name from the array into new button//
         newButton.text(initialSearch[i]);
         //appends the button to html page//
         $(placeToAdd).append(newButton);
-
-
-
 
     }
 }
@@ -34,4 +31,34 @@ $(function () {
 
 });
 
+// this targets when one of the buttons is pressed//
+$(document).on("click", ".searchButton", function () {
 
+    var type = $(this).data("type");
+
+    // console.log(type);
+
+    //API query with 10 results limit//
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=dc6zaTOxFJmzC&limit=10"
+
+    //AJAX call//
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+            // console.log(response);
+            //for loop to display gifs in html document/
+            for(var i= 0; i<response.data.length; i++){
+                //create a new div for gif picture//
+                var gifDiv = $("<div class='gif-item'>");
+                //var that calls the rating of gif
+                var rating = response.data[i].rating;
+                //var that shows the rating on a <p> tag in the html document//
+                var showRating = $("<p>").text("Rating: "+rating);
+
+            }
+
+        })
+})
